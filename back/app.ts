3f9924eb689b2035a712e53f557b43ff0064769b
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-08-21 19:55:36 +0300

    added socket events on back-end

--- back/app.ts
+++ back/app.ts
@@ -2,8 +2,22 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-io.on("connection", () => {
+io.on("connection", (socket: any) => {
   /* … */
+  socket.user = {
+    id: socket.id,
+    name: "Test " + Math.floor(Math.random() * 1000),
+  };
+
+  io.emit("auth::connect", socket.user);
+
+  socket.on("chat::sendMessage", (message: string) => {
+    io.emit("chat::receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("auth::disconnect", socket.user);
+  });
 });
 
-server.listen(3000);
+server.listen(3001);
