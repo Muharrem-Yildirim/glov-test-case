5139e7cf63c265f45f1cb35cffb2262e5ed66395
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-08-22 19:10:36 +0300

    now image is getting by id

--- front/components/commands/image-placeholder.tsx
+++ front/components/commands/image-placeholder.tsx
@@ -1,13 +1,28 @@
+import { getCaretCoordinates } from "@/lib/utils";
+import { CommandPlaceholder } from "@/types/command-placeholder";
+
 export default function ImagePlaceholder({
   commandParameters,
-}: {
-  commandParameters: string[];
-}) {
+  inputRef,
+}: CommandPlaceholder) {
+  if (!inputRef.current) return;
+  if (commandParameters.length == 0 || commandParameters[0] === "") return;
+
   return (
-    <div className="absolute bottom-5">
+    <div
+      className="absolute bottom-2 left-2 bg-black/60 rounded-xl p-2"
+      style={{
+        left: getCaretCoordinates(
+          inputRef.current,
+          inputRef.current.selectionEnd ?? 0
+        ).left,
+      }}
+    >
       <img
+        height={"100px"}
+        width={"100px"}
         alt="random"
-        src={"https://picsum.photos/200?v=" + commandParameters[0]}
+        src={"https://picsum.photos/id/" + commandParameters[0] + "/200"}
       />
     </div>
   );
