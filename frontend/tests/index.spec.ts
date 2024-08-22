b4b0db2d5374e96515d90c685c522e5b59dea3a1
Muharrem Yıldırım <muharremyildirim303@gmail.com>
2024-08-23 00:12:29 +0300

    Update index.spec.ts

--- frontend/tests/index.spec.ts
+++ frontend/tests/index.spec.ts
@@ -1,15 +1,15 @@
 import { test, expect } from "@playwright/test";
 
 test("socket server is open?", async ({ page }) => {
-  const response = await page.goto("http://localhost:3001");
-  await page.waitForURL("http://localhost:3001");
+  const response = await page.goto("http://localhost:3002");
+  await page.waitForURL("http://localhost:3002");
 
   expect(response!.status()).toBe(404);
 });
 
 test("can send message?", async ({ page }) => {
-  await page.goto("http://localhost:3000");
-  await page.waitForURL("http://localhost:3000");
+  await page.goto("http://localhost:3001");
+  await page.waitForURL("http://localhost:3001");
   await setTimeout(() => {}, 5000);
   await page.locator("input").fill("test message");
   await page.locator("input").press("Enter");
