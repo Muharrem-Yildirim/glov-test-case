02cff9af94b35c38aec577dd187c3b369f101c89
Muharrem-Yildirim <muharremyildirim303@gmail.com>
2024-08-22 23:43:03 +0300

    mobile view issue solved

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ import Chat from "@/components/chat";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-h-screen flex-col bg-[#212121] lg:pb-10 pb-2">
+    <main className="flex min-h-[calc(100dvh)] max-h-[calc(100dvh)] flex-col bg-[#212121] lg:pb-10 pb-2">
       <Chat />
     </main>
   );
